5997938e50f6fff45bcaef39be8fd15ab85acb71
Masakazu Takahashi <55798814+MAA39@users.noreply.github.com>
2023-09-14 13:41:59 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # study_JSPrimer
+
+https://jsprimer.net/intro/
+
+読み進めてます。
---
+++ 第一章/変数と宣言.js
@@ -0,0 +1,12 @@
+//日本語のタイトルになるのおもろ
+
+// 続けて設定する場合は、下の方を書かなくてもいいっていうのは知らなかったです
+const bookTitle = "JavaScript Primer",
+      bookCategory = "プログラミング";
+
+
+// 抜粋:
+// りあクト！ TypeScriptで始めるつらくないReact開発 第4版【① 言語・環境編】
+// 大岡由佳
+// https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewBook?id=0
+// この作品は著作権で保護されている可能性があります。
